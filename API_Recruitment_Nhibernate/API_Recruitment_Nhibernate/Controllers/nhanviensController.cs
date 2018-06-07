1ad93243d971478ddb5611cb2daad18bade54e5c
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-06-07 19:42:37 +0700

    Update Post for Nhanvien Recruitment Hibernate

--- API_Recruitment_Nhibernate/API_Recruitment_Nhibernate/Controllers/nhanviensController.cs
+++ API_Recruitment_Nhibernate/API_Recruitment_Nhibernate/Controllers/nhanviensController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.Http.Description;
 using API_Recruitment_Nhibernate.Models;
 using NHibernate;
 
@@ -30,8 +31,28 @@ namespace API_Recruitment_Nhibernate.Controllers
         }
 
         // POST: api/nhanviens
-        public void Post([FromBody]string value)
+        [HttpPost]
+        [ResponseType(typeof(phieutuyendung))]
+        public IHttpActionResult Post([FromBody]phieutuyendung model)
         {
+            using (ISession session = NHibertnateSession.OpenSession())
+            {
+                phieutuyendung meo = session.Get<phieutuyendung>(model.ptd_id);
+                nhanviens staff = new nhanviens();
+                staff.nv_ten = meo.ptd_ten;
+                staff.nv_ngaysinh = meo.ptd_ngaysinh;
+                staff.nv_sdt = meo.ptd_sdt;
+                staff.nv_gioitinh = meo.ptd_gioitinh;
+                staff.cvu_id = int.Parse(meo.ptd_chucvu);
+                staff.nv_email = meo.ptd_email;
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(staff);
+                    session.Delete(meo);
+                    transaction.Commit();
+                    return Ok("{status:200,message: complete Save }");
+                }
+            }
         }
 
         // PUT: api/nhanviens/5
